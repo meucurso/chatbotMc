a97a6da3f5665c1c44321b3ccb2f65ddc0c4e099
meucurso <91082675+meucurso@users.noreply.github.com>
2023-06-16 09:07:52 -0300

    Update index.js

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react";
-import logo from "../assets/logo.png";
+import logo from "../../assets/logo.png";
 import { Link as RouterLink } from "react-router-dom";
 
 import {
